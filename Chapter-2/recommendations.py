4e89a87aa9891ae52176127be61d0e591e733ccc
titus <ituzhi@163.com>
2016-05-11 10:07:22 +0800

    Add files via upload

---
+++ Chapter-2/recommendations.py
@@ -0,0 +1,64 @@
+import math
+
+critics = {
+'Lisa Rose': {'Lady in the Water': 2.5, 'Snakes on a Plane': 3.5, 'Just My Luck': 3.0, 'Superman Returns': 3.5, 'You, Me and Dupree': 2.5, 'The Night Listener': 3.0},
+'Gene Seymour': {'Lady in the Water': 3.0, 'Snakes on a Plane': 3.5, 'Just My Luck': 1.5, 'Superman Returns': 5.0, 'The Night Listener': 3.0, 'You, Me and Dupree': 3.5},
+'Michael Phillips': {'Lady in the Water': 2.5, 'Snakes on a Plane': 3.0, 'Superman Returns': 3.5, 'The Night Listener': 4.0},
+'Claudia Puig': {'Snakes on a Plane': 3.5, 'Just My Luck': 3.0, 'The Night Listener': 4.5, 'Superman Returns': 4.0, 'You, Me and Dupree': 2.5},
+'Mick LaSalle': {'Lady in the Water': 3.0, 'Snakes on a Plane': 4.0, 'Just My Luck': 2.0, 'Superman Returns': 3.0, 'The Night Listener': 3.0, 'You, Me and Dupree': 2.0},
+'Jack Matthews': {'Lady in the Water': 3.0, 'Snakes on a Plane': 4.0, 'The Night Listener': 3.0, 'Superman Returns': 5.0, 'You, Me and Dupree': 3.5},
+'Toby': {'Snakes on a Plane':4.5,'You, Me and Dupree':1.0,'Superman Returns':4.0}
+}
+
+def sim_distance(prefs, person1, person2):
+    si = {}
+    for item in prefs[person1]:
+        if item in prefs[person2]:
+            si[item] = 1
+
+    if len(si) == 0: 
+        return 0
+
+    sim_of_squares = 0
+    for item in si:
+        sim_of_squares += pow(prefs[person1][item] - prefs[person2][item], 2)
+    return 1 / (1 + sim_of_squares)
+
+
+# Returns the Person correlation coefficient
+def sim_person(prefs, p1, p2):
+    si = {}
+    for item in prefs[p1]:
+        if item in prefs[p2]:
+            si[item] = 1
+
+    n = len(si)
+
+    if n == 0:
+        return 0
+
+    sum1 = sum([prefs[p1][it] for it in si])
+    sum2 = sum([prefs[p2][it] for it in si])
+
+    sum1Sq = sum([pow(prefs[p1][it], 2) for it in si])
+    sum2Sq = sum([pow(prefs[p2][it], 2) for it in si])
+
+    pSum = sum([prefs[p1][it] * prefs[p2][it] for it in si])
+
+    num = pSum - (sum1 * sum2 / n)
+    den = math.sqrt((sum1Sq - pow(sum1, 2)/n)*(sum2Sq - pow(sum2, 2)/n))
+    if den == 0:
+        return 0
+    return num / den
+
+
+def topMatches(prefs, person, n=5, similarity=sim_person):
+    scores = [(similarity(prefs, person, other), other) 
+                  for other in prefs if other != person]
+    scores.sort()
+    scores.reverse()
+    return scores[0:n]
+
+print sim_distance(critics, 'Lisa Rose', 'Gene Seymour')
+print sim_person(critics, 'Lisa Rose', 'Gene Seymour')
+print topMatches(critics, 'Toby')
