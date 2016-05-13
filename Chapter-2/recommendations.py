2a8ae8012f6d9e12a7c75358eeea28d7457b5197
titus <ituzhi@163.com>
2016-05-13 11:30:38 +0800

    Add files via upload

--- Chapter-2/recommendations.py
+++ Chapter-2/recommendations.py
@@ -59,6 +59,31 @@ def topMatches(prefs, person, n=5, similarity=sim_person):
     scores.reverse()
     return scores[0:n]
 
-print sim_distance(critics, 'Lisa Rose', 'Gene Seymour')
-print sim_person(critics, 'Lisa Rose', 'Gene Seymour')
-print topMatches(critics, 'Toby')
+
+def getRecommendations(prefs, person, similarity=sim_person):
+    totals = {}
+    simSums = {}
+    for other in prefs:
+        if other == person: continue
+        sim = similarity(prefs, person, other)
+
+        if sim < 0: continue
+
+        for item in prefs[other]:
+            if item not in prefs[person] or prefs[person][item] == 0:
+                totals.setdefault(item, 0)
+                totals[item] += prefs[other][item] * sim
+                simSums.setdefault(item, 0)
+                simSums[item] += sim
+
+    rankings = [(total / simSums[item], item) for item, total in totals.items()]
+    rankings.sort()
+    rankings.reverse()
+    return rankings
+
+
+# print sim_distance(critics, 'Lisa Rose', 'Gene Seymour')
+# print sim_person(critics, 'Lisa Rose', 'Gene Seymour')
+# print topMatches(critics, 'Toby')
+# print getRecommendations(critics, 'Toby')
+print getRecommendations(critics, 'Toby', similarity=sim_distance)
