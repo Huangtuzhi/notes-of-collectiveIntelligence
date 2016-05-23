4b6c03df4202f1d8c2a6d4a6b8a01c94ace6d52a
titus <ituzhi@163.com>
2016-05-23 22:22:38 +0800

    Add files via upload

--- Chapter-5/optimization.py
+++ Chapter-5/optimization.py
@@ -100,12 +100,88 @@ def hillclimb(domin, costf):
 
         if best == current:
             break
-
     return sol
 
 
+def annealingoptimize(domain, costf, T=10000.0, cool=0.95, step=1):
+    vec = [random.randint(domin[i][0], domin[i][1]) 
+          for i in range(len(domin))]
+
+    while T > 0.1:
+        i = random.randint(0, len(domin) - 1)
+        dir = random.randint(-step, step)
+
+        # Create a new list with one of the values changed
+        vecb = vec[:]
+        vecb[i] += dir
+        if vecb[i] < domin[i][0]:
+            vecb[i] = domin[i][0]
+        elif vecb[i] > domin[i][1]:
+            vecb[i] = domin[i][1]
+
+        # Calculate the current cost and the new cost
+        ea = costf(vec)
+        eb = costf(vecb)
+        p = pow(math.e, (-eb-ea)/T)
+
+        if (eb < ea or random.random() < p):
+            vec = vecb
+
+        T = T*cool
+    return vec
+
+
+def geneticoptimize(domain, costf, popsize=50, step=1, mutprod=0.2, elite=0.2, maxiter=100):
+    # Mutation operation
+    def mutate(vec):
+        i = random.randint(0, len(domin) - 1)
+        if random.random() < 0.5 and vec[i] > domin[i][0]:
+            return vec[0:i] + [vec[i] - step] + vec[i+1:]
+        elif vec[i] < domin[i][1]:
+            return vec[0:i] + [vec[i] + step] + vec[i+1:]
+
+    # Crossover operation
+    def crossover(r1, r2):
+        i = random.randint(1, len(domin) - 2)
+        return r1[0:i] + r2[i:]
+
+    # Build the initial population
+    pop = []
+    for i in range(popsize):
+        vec = [random.randint(domin[i][0], domin[i][1]) 
+            for i in range(len(domin))]
+        pop.append(vec)
+
+    toplite = int(elite * popsize)
+
+    # Main loop
+    for i in range(maxiter):
+        scores = [(costf(v), v) for v in pop]
+        scores.sort()
+        ranked = [v for (s, v) in scores]
+
+        # Start with the pure winners
+        pop = ranked[0: toplite]
+
+        # Add mutated and bred forms of the winners
+        while len(pop) < popsize:
+            if random.random() < mutprod:
+                # Mutation
+                c = random.randint(0, toplite)
+                pop.append(mutate(ranked[c]))
+            else:
+                # Crossover
+                c1 = random.randint(0, toplite)
+                c2 = random.randint(0, toplite)
+                pop.append(crossover(ranked[c1], ranked[c2]))
+
+        # Print current best score
+        print scores[0][0]
+    return scores[0][1]
+
+
+
 domin = [(0, 8)]*(len(people)*2)
-print domin
-s = hillclimb(domin, schedulecost)
-print schedulecost(s)
+s = geneticoptimize(domin, schedulecost)
+print s
 printschedule(s)
