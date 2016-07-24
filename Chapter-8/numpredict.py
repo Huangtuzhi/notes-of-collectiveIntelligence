cc645d67c0a644b95c3040d53516cbe9be35e44c
titus <ituzhi@163.com>
2016-07-24 22:34:24 +0800

    Add files via upload

--- Chapter-8/numpredict.py
+++ Chapter-8/numpredict.py
@@ -28,6 +28,37 @@ def wineset1():
 		# Add to the dataset
 		rows.append({'input': (rating, age),
 			'result': price})
-		return rows
+	return rows
 
-print wineset1()
+def eucliden(v1, v2):
+	d = 0.0
+	for i in range(len(v1)):
+		d += (v1[i] - v2[i])**2
+	return math.sqrt(d)
+
+def getdistances(data, vec1):
+	distancelist = []
+	for i in range(len(data)):
+		vec2 = data[i]['input']
+		distancelist.append((eucliden(vec1, vec2), i))
+	distancelist.sort()
+	return distancelist
+
+def knnestimate(data, vec1, k=3):
+	# Get sorted distances
+	dlist = getdistances(data, vec1)
+	avg = 0.0
+
+	# Take the average of the top K results
+	for i in range(k):
+		idx = dlist[i][1]
+		avg += data[idx]['result']
+	avg = avg / k
+	return avg
+
+data= wineset1()
+print knnestimate(data, (95.0, 3.0))
+print knnestimate(data, (99.0, 3.0))
+print knnestimate(data, (99.0, 5.0))
+print wineprice(99.0, 5.0)
+print knnestimate(data, (99.0, 5.0), k=1)
